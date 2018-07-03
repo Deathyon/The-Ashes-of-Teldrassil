6dfa0f078e4ecc3e68a2fc1ad6cf1bffc59b28e6
Adam Clark <adamtclark@knights.ucf.edu>
2018-07-03 11:33:11 -0400

    php test

---
+++ action.php
@@ -0,0 +1,13 @@
+<?php
+if( isset($_POST['firstname'] ) && isset( $_POST['lastname'] ) )
+{
+    $txt= $_POST['firstname'].' - '.$_POST['lastname'] . PHP_EOL; 
+    file_put_contents('fields.txt', $txt, FILE_APPEND);
+}
+?><?php
+if( isset($_POST['firstname'] ) && isset( $_POST['lastname'] ) )
+{
+    $txt= $_POST['firstname'].' - '.$_POST['lastname'] . PHP_EOL; 
+    file_put_contents('fields.txt', $txt, FILE_APPEND);
+}
+?>
